c527c128ce75c533e5b6c674d172207828124f5f
andrei1010 <andrei@andrei1010.me>
2025-01-08 21:20:00 +0100

    Create config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    oh-my-posh init fish --config 'https://raw.githubusercontent.com/andr3i1010/dotfiles/refs/heads/main/.posh/andrei1010.omp.json' | source
+    neofetch
+end
